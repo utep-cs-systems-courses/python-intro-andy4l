01801a5c332087de29c68e39fc3c7e5015604d1d
andy4l <andyramos84@gmail.com>
2020-08-30 15:47:56 -0600

    commented code, correct file output

--- wordCount.py
+++ wordCount.py
@@ -1,13 +1,9 @@
 import sys  # cmd args
 import string
 
+# arguments passed while executing program in shell
 ffname = sys.argv[1]
 oname = sys.argv[2]
-
-
-def sortDict(words):
-    words = sorted(words.keys(), key=lambda x: x.lower())
-    return words
 
 
 def fileReader(filename, outputfile):
@@ -15,27 +11,26 @@ def fileReader(filename, outputfile):
     o = open(outputfile, "w")
     w = dict()
     for line in f:
+        # remove special characters that are integrated in words
         line = line.strip()
-
-        words = line.split(" ")
-        # print(words)
+        line = line.replace('-', " ")
+        line = line.replace("'", " ")
+        # split words into list
+        words = line.split()
 
         for word in words:
+            # eliminate punctuation in each word
             word = word.translate(str.maketrans('', '', string.punctuation))
+            word = word.lower()
             if word in w:
                 w[word] = w[word] + 1
-            # if word in punc:
-            #   words = words.
             else:
                 w[word] = 1
 
-    #   iterate and sort alphabetically through dictionary
+    #   sorted used to sort keys in dictionary
     #   write dictionary output key by key to output file
-    #sortedWords = sortDict(words)
     for key in sorted(w.keys()):
-    #for key in sortedWords:
         o.write(key + " " + str(w[key]) + "\n")
-        print(key, " ", w[key])
     f.close()
     o.close()
 
